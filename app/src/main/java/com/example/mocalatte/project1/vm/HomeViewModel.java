10c5ec02aa52802cd80ed9d033dce98bc5fede5d
mocalatte <08dbwjd@naver.com>
2018-11-20 12:53:19 +0900

    log in main

---
+++ app/src/main/java/com/example/mocalatte/project1/vm/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mocalatte.project1.vm;
+
+import android.view.View;
+
+public class HomeViewModel extends ViewModel<HomeViewModel> {
+    public HomeViewModel(View view) {
+        super(view);
+    }
+
+    @Override
+    public void binding() {
+
+    }
+
+    @Override
+    public void initialization(HomeViewModel data) {
+
+    }
+}
